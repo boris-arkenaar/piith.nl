0701f4ba2e372cdf12eed239e5ef71e6b7b323e2
Boris Arkenaar <boris@arkenaar.com>
2020-12-21 15:50:02 +0100

    Add next.js

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Piith | Professionele Integratieve Interactieve Therapeuten</title>
+        <link rel="icon" type="image/png" href="/icon.png" />
+      </Head>
+      <main>
+        Body
+      </main>
+    </div>
+  );
+}
