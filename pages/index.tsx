0e97a4bbcfbe1d8c4cd791ed2af84326fc7be5b6
Boris Arkenaar <boris@arkenaar.com>
2020-12-22 08:04:53 +0100

    Add eslint and prettier

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,15 @@
 import Head from "next/head";
 
-export default function Home() {
+export default function Home(): JSX.Element {
   return (
     <div>
       <Head>
-        <title>Piith | Professionele Integratieve Interactieve Therapeuten</title>
+        <title>
+          Piith | Professionele Integratieve Interactieve Therapeuten
+        </title>
         <link rel="icon" type="image/png" href="/icon.png" />
       </Head>
-      <main>
-        Body
-      </main>
+      <main>Body</main>
     </div>
   );
 }
