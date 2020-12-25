1ff139283b5767ef34e437da7fdd621d095428d0
Boris Arkenaar <boris@arkenaar.com>
2020-12-25 10:30:13 +0100

    Show last three articles on home page

--- content/articles/2019-09-07-article-drie.md
+++ content/articles/2019-09-07-article-drie.md
@@ -8,6 +8,8 @@ Quisque eu libero in massa dictum varius eu quis nisl. Quisque sodales ante non 
 
 Vivamus congue sagittis felis. Proin ullamcorper massa eu est auctor, sed feugiat dui rutrum. Nullam placerat hendrerit ligula at volutpat. Nunc eros elit, faucibus at ipsum nec, vulputate ultricies tortor. Mauris placerat elementum felis, at bibendum massa dictum vel. Cras eu vestibulum nunc. Nam sed hendrerit erat. Donec sed ligula orci. Mauris efficitur rutrum sem, nec convallis magna porttitor ac. Mauris mollis vitae diam ac rhoncus. Fusce ante elit, venenatis sed tellus quis, feugiat molestie elit. Sed blandit enim a elit luctus, in cursus nunc ornare.
 
+<!--more-->
+
 Fusce congue mi et pulvinar facilisis. Curabitur pretium ac felis at porttitor. Maecenas et metus eleifend velit pulvinar placerat id vitae urna. In rhoncus molestie ipsum et viverra. Nunc tempus consequat nisi, mollis faucibus lectus hendrerit convallis. Integer et luctus nibh, sed tempor sem. Suspendisse finibus velit nisl, eget tempor lacus vehicula vel. Vivamus nec dapibus risus. Cras congue libero in molestie efficitur. Morbi porta, justo ut facilisis bibendum, elit tellus condimentum purus, quis consequat mauris sapien sit amet ligula. Vestibulum dictum quam sit amet finibus placerat. Cras a quam at lacus laoreet hendrerit. Proin nec velit felis. Donec et turpis at felis dictum pharetra eget sit amet ligula.
 
 Pellentesque vulputate ornare dui vel condimentum. Pellentesque libero elit, luctus lobortis vehicula a, scelerisque in magna. Donec tincidunt tempus massa nec condimentum. Quisque leo enim, lacinia quis finibus et, sodales mollis lectus. Pellentesque in accumsan mauris. Morbi quis erat dui. Sed in neque velit. Mauris elementum blandit rutrum.
--- pages/index.tsx
+++ pages/index.tsx
@@ -9,6 +9,34 @@ import html from "remark-html";
 import Layout from "../components/layout";
 
 const home = join(process.cwd(), "content/home.md");
+const postsDirectory = join(process.cwd(), "content/articles");
+
+const getPostData = async (fileName) => {
+  const id = fileName.replace(/\.md$/, "");
+  const fullPath = join(postsDirectory, fileName);
+  const rawContent = fs.readFileSync(fullPath, "utf8");
+  const frontMatter = matter(rawContent);
+  const splitContent = frontMatter.content.split("<!--more-->");
+  const introContent = splitContent[0];
+  const hasMore = splitContent.length > 1;
+
+  const processedContent = await remark().use(html).process(introContent);
+  const content = processedContent.toString();
+
+  return {
+    content,
+    date: frontMatter.data.date.toISOString(),
+    hasMore,
+    id,
+    title: frontMatter.data.title,
+  };
+};
+
+const getPostsData = async () => {
+  const fileNames = fs.readdirSync(postsDirectory);
+
+  return Promise.all(fileNames.slice(-3).reverse().map(getPostData));
+};
 
 export const getStaticProps: GetStaticProps = async () => {
   const rawContent = fs.readFileSync(home, "utf8");
@@ -22,6 +50,7 @@ export const getStaticProps: GetStaticProps = async () => {
   return {
     props: {
       content: htmlContent,
+      posts: await getPostsData(),
       title: frontMatter.data.title,
     },
   };
@@ -29,10 +58,11 @@ export const getStaticProps: GetStaticProps = async () => {
 
 type HomeProps = {
   content: string;
+  posts: any;
   title: string;
 };
 
-const Home: React.FC<HomeProps> = ({ content, title }) => (
+const Home: React.FC<HomeProps> = ({ content, posts, title }) => (
   <Layout>
     <Head>
       <title>{title}</title>
@@ -40,6 +70,22 @@ const Home: React.FC<HomeProps> = ({ content, title }) => (
       <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
     </Head>
     <div dangerouslySetInnerHTML={{ __html: content }} />
+    {posts.map((post) => (
+      <article key={post.id}>
+        <header>
+          <h1>
+            <a href={`/${post.id}`}>{post.title}</a>
+          </h1>
+        </header>
+        <div dangerouslySetInnerHTML={{ __html: post.content }} />
+        {post.hasMore && (
+          <a className="more-link" href={`/${post.id}#lees-verder`}>
+            Lees verder →
+          </a>
+        )}
+        <footer className="entry-meta">{post.date}</footer>
+      </article>
+    ))}
   </Layout>
 );
 
